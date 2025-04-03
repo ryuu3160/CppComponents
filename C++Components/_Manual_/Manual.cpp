3cf66d96f436baf01fe9217cc6580d2c4a42731d
ryuu3160(MainPC) <principe2580613@yahoo.co.jp>
2025-04-04 03:14:09 +0900

    feat: マニュアルの追加

---
+++ C++Components/_Manual_/Manual.cpp
@@ -0,0 +1,21 @@
+/*+===================================================================
+	File: Manual.cpp
+	Summary: 利用方法のサンプル
+	Author: ryuu3160
+	Date: 2025/4/4 初回作成
+===================================================================+*/
+
+// ==============================
+//	include
+// ==============================
+#include "SingletonManual.hpp"
+#include <iostream>
+
+int main()
+{
+	SingletonTest();
+
+
+	static_cast<void>(getchar());
+	return 0;
+}
